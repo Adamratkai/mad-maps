4e638254fb3efdc886c8c33bd4ef6367264fbed6
Adamratkai <153464879+Adamratkai@users.noreply.github.com>
2025-02-19 13:35:03 +0100

    init backend spring boot

---
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/MadMapsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.madmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadMapsApplication.class, args);
+	}
+
+}
