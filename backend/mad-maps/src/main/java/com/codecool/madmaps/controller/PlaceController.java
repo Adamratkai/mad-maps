a121dbfe2a16898dd327a6776b679d4a87992722
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-02-20 14:35:27 +0100

    Implement getAll endpoint

--- backend/mad-maps/src/main/java/com/codecool/madmaps/controller/PlaceController.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/controller/PlaceController.java
@@ -1,8 +1,26 @@
 package com.codecool.madmaps.controller;
 
 
+import com.codecool.madmaps.DTO.Place.PlaceDTO;
+import com.codecool.madmaps.service.PlaceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("api/places")
 public class PlaceController {
+
+    PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/")
+    public List<PlaceDTO> getPlaces() {
+        return placeService.getAllPlaces();
+    }
 }
