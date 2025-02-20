5f53edc419482fd159a8888bfc3b7180977cbbf0
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-02-20 10:17:58 +0100

    create post endpoint for TripDay

--- backend/mad-maps/src/main/java/com/codecool/madmaps/controller/TripDayController.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/controller/TripDayController.java
@@ -1,8 +1,23 @@
 package com.codecool.madmaps.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.madmaps.DTO.TripDay.TripDayCreateDTO;
+import com.codecool.madmaps.DTO.TripDay.TripDayDTO;
+import com.codecool.madmaps.service.TripDayService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/tripdays")
 public class TripDayController {
+    private final TripDayService tripDayService;
+
+    public TripDayController(TripDayService tripDayService) {
+        this.tripDayService = tripDayService;
+    }
+
+    @PostMapping("/")
+    public TripDayCreateDTO createTripDay(@RequestBody TripDayCreateDTO newTripDay) {
+        this.tripDayService.createTripDay(newTripDay);
+        return newTripDay;
+    }
 }
