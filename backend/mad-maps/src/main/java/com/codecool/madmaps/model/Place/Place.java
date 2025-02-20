d8f7d4388b6b34bd6c35d20e750b8dd9ca21f82c
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-02-20 14:35:43 +0100

    Implement place model

--- backend/mad-maps/src/main/java/com/codecool/madmaps/model/Place/Place.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/model/Place/Place.java
@@ -1,4 +1,7 @@
 package com.codecool.madmaps.model.Place;
 
-public record Place() {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Place(UUID publicId, String name, double rating, double price, String img, UUID tripId, LocalDateTime time) {
 }
