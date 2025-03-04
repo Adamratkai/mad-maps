e03e958c3c7cd0b9529b2cc684d9f71ff9aecd42
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-04 22:04:58 +0100

    create TripActivity entity

---
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/model/TripActivity/TripActivity.java
@@ -0,0 +1,29 @@
+package com.codecool.madmaps.model.TripActivity;
+
+import com.codecool.madmaps.model.Place.Place;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "trip_activities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TripActivity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    private LocalDateTime visitTime;
+}
