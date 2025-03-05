afbd1330289d119116ffb67ae4fae110cecb3cf6
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-05 22:20:59 +0100

    create Trip entity

--- backend/mad-maps/src/main/java/com/codecool/madmaps/model/Trip/Trip.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/model/Trip/Trip.java
@@ -1,9 +1,40 @@
 package com.codecool.madmaps.model.Trip;
 
 
+import com.codecool.madmaps.model.Traveler.Traveller;
+import com.codecool.madmaps.model.TripActivity.TripActivity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
-public record Trip(UUID publicId, String name, List<UUID> tripDayIds, LocalDate startDate, LocalDate endDate) {
+@Entity
+@Table(name = "trips")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @OneToMany(mappedBy = "trip", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<TripActivity> tripActivities;
+
+    @ManyToOne
+    @JoinColumn(name = "traveller_id", nullable = false)
+    private Traveller traveller;
+
 }
