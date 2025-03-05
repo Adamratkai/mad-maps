bc69f652a88d1c735d66e0a2f77d54266a1e5169
Adamratkai <153464879+Adamratkai@users.noreply.github.com>
2025-03-05 17:58:39 +0100

    added react router and navbar

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,19 +1,30 @@
 import './App.css'
-import GoogleMapComponent from './components/GoogleMapComponent'
-import TripMenu from "./components/tripmenu/TripMenu.jsx";
+import Navbar from "./components/Navbar.jsx";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import HomePage from "./components/HomePage.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
+import Trip from "./components/Trip.jsx";
+
+
+function Layout({children}) {
+    return (
+        <>
+            <Navbar/>
+            {children}
+        </>
+    );
+}
+
+const router = createBrowserRouter([
+    {path: "/", element: <Layout><HomePage/></Layout>},
+    {path: "/trip", element: <Layout><Trip/></Layout>},
+    {path: "*", element: <ErrorPage/>}
+]);
 
 function App() {
-
     return (
-        <div className="App">
-            <div className="GoogleMap">
-                <GoogleMapComponent/>
-            </div>
-            <div className="TripMenu">
-                <TripMenu/>
-            </div>
-        </div>
-    )
+        <RouterProvider router={router}></RouterProvider>
+    );
 }
 
 export default App
