da96d8e8169f65bff15008f978be7ee0bac326dc
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-06 09:50:57 +0100

    override equals and hashcode method for PlaceType entity

--- backend/mad-maps/src/main/java/com/codecool/madmaps/model/PlaceType/PlaceType.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/model/PlaceType/PlaceType.java
@@ -1,10 +1,7 @@
 package com.codecool.madmaps.model.PlaceType;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "place_types")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class PlaceType {
 
     @Id
