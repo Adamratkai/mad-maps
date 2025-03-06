306c5dfcfd0bb050d17a769931adc10c2d58d965
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-06 09:37:43 +0100

    create TripRepository

---
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/repository/TripRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.madmaps.repository;
+
+import com.codecool.madmaps.model.Trip.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, Long> {
+}
