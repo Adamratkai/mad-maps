f73337cd93c9bff007c16156eb4f8fbf26b7232c
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-06 21:28:14 +0100

    add custom method to PlaceTypeRepository

--- backend/mad-maps/src/main/java/com/codecool/madmaps/repository/PlaceTypeRepository.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/repository/PlaceTypeRepository.java
@@ -4,6 +4,9 @@ import com.codecool.madmaps.model.PlaceType.PlaceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlaceTypeRepository extends JpaRepository<PlaceType, Long> {
+    Optional<PlaceType> findByPlaceType(String placeType);
 }
