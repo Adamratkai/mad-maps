0d2658d2d815d4ea61e82f0c75b8c77a309e348f
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-06 21:08:43 +0100

    add Transactional annotation and TripActivity Repository

--- backend/mad-maps/src/main/java/com/codecool/madmaps/service/TripService.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/service/TripService.java
@@ -7,7 +7,9 @@ import com.codecool.madmaps.model.Place.Place;
 import com.codecool.madmaps.model.Trip.Trip;
 import com.codecool.madmaps.model.TripActivity.TripActivity;
 import com.codecool.madmaps.repository.PlaceRepository;
+import com.codecool.madmaps.repository.TripActivityRepository;
 import com.codecool.madmaps.repository.TripRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,10 +22,12 @@ public class TripService {
 
     private final TripRepository tripRepository;
     private final PlaceRepository placeRepository;
+    private final TripActivityRepository tripActivityRepository;
 
-    public TripService(TripRepository tripRepository, PlaceRepository placeRepository) {
+    public TripService(TripRepository tripRepository, PlaceRepository placeRepository, TripActivityRepository tripActivityRepository) {
         this.tripRepository = tripRepository;
         this.placeRepository = placeRepository;
+        this.tripActivityRepository = tripActivityRepository;
     }
 
     public UUID createTrip(TripCreateDTO tripCreateDTO) {
@@ -35,13 +39,16 @@ public class TripService {
         return this.tripRepository.save(newTrip).getTripId();
     }
 
-    private void addTripActivity(TripActivityCreateDTO tripActivityCreateDTO) {
+
+    @Transactional
+    public void addTripActivity(TripActivityCreateDTO tripActivityCreateDTO) {
         TripActivity newTripActivity = new TripActivity();
         newTripActivity.setVisitTime(tripActivityCreateDTO.visitTime());
-        Trip trip = this.tripRepository.findByTripId(tripActivityCreateDTO.tripId()).orElseThrow(NoSuchElementException::new);
+        Trip trip = this.tripRepository.findByTripId(tripActivityCreateDTO.tripId()).orElseThrow(() -> new NoSuchElementException("Trip not found"));
         newTripActivity.setTrip(trip);
-        Place place = this.placeRepository.findByPlaceId(tripActivityCreateDTO.placeId()).orElseThrow(NoSuchElementException::new);
+        Place place = this.placeRepository.findByPlaceId(tripActivityCreateDTO.placeId()).orElseThrow(() -> new NoSuchElementException("Place not found"));
         newTripActivity.setPlace(place);
+        this.tripActivityRepository.save(newTripActivity);
     }
 
     public List<TripDTO> getTrips() {
