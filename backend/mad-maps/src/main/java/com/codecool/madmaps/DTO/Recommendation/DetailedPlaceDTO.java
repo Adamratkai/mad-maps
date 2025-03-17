d7660307b71429cdf998c90d2227c09b562ecbf9
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-17 14:09:52 +0100

    Change rating and price_level type

--- backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Recommendation/DetailedPlaceDTO.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Recommendation/DetailedPlaceDTO.java
@@ -2,5 +2,5 @@ package com.codecool.madmaps.DTO.Recommendation;
 
 import java.util.List;
 
-public record DetailedPlaceDTO(String name, String rating, String price_level, String place_id, List<PlacePhotoDTO> photos, List<String> types, PlaceOpeningHours opening_hours ) {
+public record DetailedPlaceDTO(String name, double rating, int price_level, String place_id, List<PlacePhotoDTO> photos, List<String> types, PlaceOpeningHours opening_hours ) {
 }
