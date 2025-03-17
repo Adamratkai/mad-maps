5e11db7413225880a233ac820bf12d22af03a566
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-17 14:15:41 +0100

    move getDetailedPlace to PlaceService and refactor getPhoto

--- backend/mad-maps/src/main/java/com/codecool/madmaps/service/RecommendationService.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/service/RecommendationService.java
@@ -1,11 +1,11 @@
 package com.codecool.madmaps.service;
 
-import com.codecool.madmaps.DTO.Recommendation.*;
+import com.codecool.madmaps.DTO.Recommendation.RecommendationDTO;
+import com.codecool.madmaps.DTO.Recommendation.RecommendationResultDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,19 +28,9 @@ public class RecommendationService {
         return response.results().stream().limit(maxResults).collect(Collectors.toList());
     }
 
-    public DetailedPlaceWithImgDTO getDetailedPlace(String place_id) {
-        String fields = "name,place_id,rating,price_level,photos,opening_hours/weekday_text,types";
-        String url = String.format("https://maps.googleapis.com/maps/api/place/details/json?fields=%s&place_id=%s&key=%s",
-                fields, place_id, googleMapsApiKey);
-        DetailedPlaceResultDTO response = restTemplate.getForObject(url, DetailedPlaceResultDTO.class);
-        DetailedPlaceDTO detailedPlace = response.result();
-        return new DetailedPlaceWithImgDTO(detailedPlace, getPhoto(detailedPlace.photos().get(0).photo_reference()));
-    }
-
-    private String getPhoto(String photo_reference) {
+    private byte[] getPhoto(String photo_reference) {
         String url = String.format("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=%s&key=%s", photo_reference, googleMapsApiKey);
-        byte[] result = restTemplate.getForObject(url, byte[].class);
-        return Base64.getEncoder().encodeToString(result);
+        return restTemplate.getForObject(url, byte[].class);
     }
 
 }
