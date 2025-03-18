d621f3d25624598bcc8c426c5f6f87e2215d1cc7
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-18 14:07:05 +0100

    add custom delete method to TripActivityRepository

--- backend/mad-maps/src/main/java/com/codecool/madmaps/repository/TripActivityRepository.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/repository/TripActivityRepository.java
@@ -1,9 +1,14 @@
 package com.codecool.madmaps.repository;
 
+import com.codecool.madmaps.model.Trip.Trip;
 import com.codecool.madmaps.model.TripActivity.TripActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface TripActivityRepository extends JpaRepository<TripActivity, Long> {
+    int deleteByTripActivityId(UUID tripActivityId);
 }
