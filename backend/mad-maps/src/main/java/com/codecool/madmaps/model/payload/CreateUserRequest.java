cd1c0c6cd43b09dec83ecbb1b11d58d3666077e3
Adamratkai <153464879+Adamratkai@users.noreply.github.com>
2025-03-18 11:51:02 +0100

    add user requests

---
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/model/payload/CreateUserRequest.java
@@ -0,0 +1,12 @@
+package com.codecool.madmaps.model.payload;
+
+import lombok.Data;
+
+@Data
+public class CreateUserRequest {
+    private String username;
+    private String password;
+    private String email;
+
+
+}
