6996f1bfb6e233745691293e38d00e49e83ebcfc
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-18 13:22:04 +0100

    extend PlaceCreateDTO with photos

--- backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Place/PlaceCreateDTO.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Place/PlaceCreateDTO.java
@@ -1,6 +1,8 @@
 package com.codecool.madmaps.DTO.Place;
 
+import com.codecool.madmaps.model.Photo.Photo;
+
 import java.util.List;
 
-public record PlaceCreateDTO(String placeId, String name, List<String> placeTypes, double rating, int priceLevel, List<String> openingHours) {
+public record PlaceCreateDTO(String placeId, String name, List<String> placeTypes, double rating, int priceLevel, List<String> openingHours, List<Photo> photos) {
 }
