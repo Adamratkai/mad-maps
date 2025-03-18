1e2db73e6dd79927e214a2e4ba717b59611193f9
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-18 14:20:21 +0100

    remove tripDays field from TripUpdateDTO

--- backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Trip/TripUpdateDTO.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Trip/TripUpdateDTO.java
@@ -5,5 +5,5 @@ import com.codecool.madmaps.DTO.TripDay.TripDayDTO;
 import java.time.LocalDate;
 import java.util.List;
 
-public record TripUpdateDTO(String name, List<TripDayDTO> tripDays, LocalDate startDate, LocalDate endDate) {
+public record TripUpdateDTO(String name, LocalDate startDate, LocalDate endDate) {
 }
