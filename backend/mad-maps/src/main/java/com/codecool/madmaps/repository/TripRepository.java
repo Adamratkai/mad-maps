9ca428d710947b506f828d375e1de3a3d65c1eeb
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-31 15:25:42 +0200

    implement getTripsByTraveller endpoint

--- backend/mad-maps/src/main/java/com/codecool/madmaps/repository/TripRepository.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/repository/TripRepository.java
@@ -1,10 +1,12 @@
 package com.codecool.madmaps.repository;
 
+import com.codecool.madmaps.model.Traveler.Traveller;
 import com.codecool.madmaps.model.Trip.Trip;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +15,5 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
     @EntityGraph(attributePaths = {"tripActivities"})
     Optional<Trip> findByTripId(UUID tripId);
     int deleteByTripId(UUID tripId);
+    List<Trip> findByTraveller(Traveller traveller);
 }
