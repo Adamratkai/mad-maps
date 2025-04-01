93d38bcd893542566c8548d852ccdc96a7f925a3
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-04-01 17:54:32 +0200

    extend place dtos

--- backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Place/PlaceDTO.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/Place/PlaceDTO.java
@@ -1,8 +1,10 @@
 package com.codecool.madmaps.DTO.Place;
 
+import com.codecool.madmaps.DTO.Recommendation.Location;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record PlaceDTO(String placeId, String name, double rating, int priceLevel, List<String> openingHours, List<UUID> photos) {
+public record PlaceDTO(String placeId, String name, double rating, int priceLevel, List<String> openingHours, List<UUID> photos, Location location) {
 }
