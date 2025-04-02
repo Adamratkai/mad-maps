775cdef2e86b37cf84eea3fd6c0a3044cdcc5ba8
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-04-02 13:34:21 +0200

    fix integration test init

--- backend/mad-maps/src/test/java/com/codecool/madmaps/integration/UserRegisterLoginIT.java
+++ backend/mad-maps/src/test/java/com/codecool/madmaps/integration/UserRegisterLoginIT.java
@@ -1,21 +1,72 @@
 package com.codecool.madmaps.integration;
 
+import com.codecool.madmaps.model.payload.CreateUserRequest;
+import com.codecool.madmaps.repository.RoleRepository;
+import com.codecool.madmaps.repository.TravellerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureMockMvc
+@Testcontainers
 public class UserRegisterLoginIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TravellerRepository travellerRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0")
+            .withDatabaseName("test")
+            .withUsername("testUser")
+            .withPassword("testPassword");
 
     @BeforeAll
     static void loadEnvVariables() {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         System.setProperty("API_KEY", dotenv.get("API_KEY"));
+    }
+
+    @Test
+    void testRegisterUserWhenUserIsValidThenResultIsUserSavedInDB () throws Exception {
+        CreateUserRequest registerRequest = new CreateUserRequest();
+        registerRequest.setUsername("testUser");
+        registerRequest.setEmail("test@example.com");
+        registerRequest.setPassword("password123");
+
+        mockMvc.perform(post("/api/traveller/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(registerRequest)))
+                .andExpect(status().isCreated());
+
+        assertThat(travellerRepository.findByEmail("test@example.com")).isPresent();
     }
 
 }
