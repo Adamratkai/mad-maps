cb80597ad839d9b72e29c341ae18a9269e974eda
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-05 17:26:15 +0200

    remove files

--- backend/mad-maps/src/main/java/com/codecool/madmaps/DTO/TripDay/TripDayCreateDTO.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.madmaps.DTO.TripDay;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
-
-public record TripDayCreateDTO(LocalDate date,  List<UUID> placeIds) {
-}
--- backend/mad-maps/src/main/java/com/codecool/madmaps/model/TripDay/TripDay.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.madmaps.model.TripDay;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
-
-public record TripDay(UUID publicId, LocalDate date, List<UUID> placeIds) {
-}
