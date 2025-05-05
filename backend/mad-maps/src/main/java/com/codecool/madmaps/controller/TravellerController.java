515589b11dd2e968a38a04ee09a95e70f706d64a
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-05 17:24:41 +0200

    refactor file and remove unused imports

--- backend/mad-maps/src/main/java/com/codecool/madmaps/controller/TravellerController.java
+++ backend/mad-maps/src/main/java/com/codecool/madmaps/controller/TravellerController.java
@@ -1,13 +1,11 @@
 package com.codecool.madmaps.controller;
 
-import com.codecool.madmaps.DTO.Jwt.JwtResponse;
-import com.codecool.madmaps.model.payload.CreateUserRequest;
-import com.codecool.madmaps.model.payload.UserRequest;
-import com.codecool.madmaps.security.jwt.JwtUtils;
+import com.codecool.madmaps.DTO.Jwt.JwtDTO;
+import com.codecool.madmaps.DTO.Traveller.CreateUserDTO;
+import com.codecool.madmaps.DTO.Traveller.UserDTO;
 import com.codecool.madmaps.service.TravellerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,24 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class TravellerController {
 
     private final TravellerService travellerService;
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtils jwtUtils;
 
-    public TravellerController(TravellerService travellerService, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+    public TravellerController(TravellerService travellerService) {
         this.travellerService = travellerService;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtils = jwtUtils;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Void> createUser(@RequestBody CreateUserRequest signUpRequest) {
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserDTO signUpRequest) {
         travellerService.registerUser(signUpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 
     @PostMapping("/login")
-    public JwtResponse loginUser(@RequestBody UserRequest loginRequest) {
+    public JwtDTO loginUser(@RequestBody UserDTO loginRequest) {
         return travellerService.loginUser(loginRequest);
     }
 
