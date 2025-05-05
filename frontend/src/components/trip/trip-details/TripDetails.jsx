d6e6ae5e2214b8d35efa6e6188d1316625995f79
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-05 17:26:29 +0200

    format file

--- frontend/src/components/trip/trip-details/TripDetails.jsx
+++ frontend/src/components/trip/trip-details/TripDetails.jsx
@@ -1,6 +1,6 @@
 import TripActivity from "../trip-activity/TripActivity.jsx";
 
-function TripDetails({ tripDetail: { tripName, startDate, endDate }, activities }) {
+function TripDetails({tripDetail: {tripName, startDate, endDate}, activities}) {
     return (
         <div className="w-[400px] h-[500px] overflow-auto p-4 border rounded-xl shadow-md bg-my-color">
             <h2 className="text-xl text-gray-500 font-semibold">{tripName}</h2>
@@ -8,7 +8,7 @@ function TripDetails({ tripDetail: { tripName, startDate, endDate }, activities 
             <div className="recommendation-container mt-4">
                 <div className="flex flex-col gap-3">
                     {activities.map((activity) => (
-                        <TripActivity key={activity.placeDTO.name} tripActivity={activity} />
+                        <TripActivity key={activity.placeDTO.name} tripActivity={activity}/>
                     ))}
                 </div>
             </div>
