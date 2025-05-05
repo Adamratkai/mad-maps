bcd7bf16b9d095ccdc35f261d028f738005353b7
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-05 17:20:25 +0200

    remove unused imports

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import {useContext, useState} from "react";
 import {useNavigate} from "react-router";
 import {AuthContext} from "../components/AuthProvider.jsx";
 
