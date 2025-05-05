bcc52ecbdf94cde5e7731134db6a791d3c663ef4
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-05 17:19:31 +0200

    format file and remove unused imports

--- frontend/src/components/trip/recommendation/Recommendation.jsx
+++ frontend/src/components/trip/recommendation/Recommendation.jsx
@@ -1,68 +1,70 @@
-import { useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import RecommendedPlace from "./RecommendedPlace.jsx";
 import RecommendationDetailedPlace from "./RecommendationDetailedPlace.jsx";
-import useAxios from "../../useAxios.js";
+import useAxios from "../../../hooks/useAxios.js";
 
 function Recommendation({location, onAddPlace, placeType}) {
-  const [recommendations, setRecommendations] = useState([]);
-  const [selectedPlaceId, setSelectedPlaceId] = useState(null);
-  const axiosInstance = useAxios();
+    const [recommendations, setRecommendations] = useState([]);
+    const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+    const axiosInstance = useAxios();
 
-  useEffect(() => {
-    const abortController = new AbortController();
-    async function fetchRecommendations () {
-      try {
+    useEffect(() => {
+        const abortController = new AbortController();
 
-        const response = await axiosInstance.get(`/api/recommendations/`, {
-          params: {
-            location: `${location.lat},${location.lng}`,
-            type: placeType,
-          },
-          signal: abortController.signal,
-        });
-        setRecommendations(response.data);
-      } catch (error) {
-        console.error("Error fetching recommendations:", error);
-      }
+        async function fetchRecommendations() {
+            try {
+
+                const response = await axiosInstance.get(`/api/recommendations/`, {
+                    params: {
+                        location: `${location.lat},${location.lng}`,
+                        type: placeType,
+                    },
+                    signal: abortController.signal,
+                });
+                setRecommendations(response.data);
+            } catch (error) {
+                console.error("Error fetching recommendations:", error);
+            }
+        }
+
+        fetchRecommendations();
+        return () => {
+            abortController.abort();
+        };
+    }, [location.lat, location.lng]);
+
+    function handlePlaceClick(placeId) {
+        setSelectedPlaceId(placeId);
     }
-    fetchRecommendations();
-    return () => {
-      abortController.abort();
-    };
-  }, [location.lat, location.lng]);
 
-  function handlePlaceClick(placeId) {
-    setSelectedPlaceId(placeId);
-  }
-
-  function handleCloseDetailedPlace() {
-    setSelectedPlaceId(null);
-  }
+    function handleCloseDetailedPlace() {
+        setSelectedPlaceId(null);
+    }
 
     return (
         <>
-          {recommendations && (
-              <div className="recommendation-container overflow-x-auto px-4 box-border">
-                <div className="flex gap-5">
-                  {recommendations.map((recommendation) => (
-                      <RecommendedPlace
-                          key={recommendation.place_id}
-                          name={recommendation.name}
-                          price={recommendation.price_level}
-                          rating={recommendation.rating}
-                          onPlaceClick={() => handlePlaceClick(recommendation.place_id)}
-                      />
-                  ))}
+            {recommendations && (
+                <div className="recommendation-container overflow-x-auto px-4 box-border">
+                    <div className="flex gap-5">
+                        {recommendations.map((recommendation) => (
+                            <RecommendedPlace
+                                key={recommendation.place_id}
+                                name={recommendation.name}
+                                price={recommendation.price_level}
+                                rating={recommendation.rating}
+                                onPlaceClick={() => handlePlaceClick(recommendation.place_id)}
+                            />
+                        ))}
+                    </div>
                 </div>
-              </div>
-          )}
-          {selectedPlaceId && (
-              <RecommendationDetailedPlace
-                  placeId={selectedPlaceId}
-                  onPlaceClose={handleCloseDetailedPlace}
-                  onAddPlace={onAddPlace}
-              />
-          )}
+            )}
+            {selectedPlaceId && (
+                <RecommendationDetailedPlace
+                    placeId={selectedPlaceId}
+                    onPlaceClose={handleCloseDetailedPlace}
+                    onAddPlace={onAddPlace}
+                />
+            )}
         </>
     );
 }
